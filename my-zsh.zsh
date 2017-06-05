2d48889917f936a827cfbedef2fc12e3c558aa4c
Roman Gorel <romangorel@gmail.com>
2017-06-06 01:21:02 +0200

    zsh stuff

---
+++ my-zsh.zsh
@@ -0,0 +1,26 @@
+export EDITOR=vim
+
+source ~/.oh-my-zsh/custom/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 
+source ~/.oh-my-zsh/custom/zsh-history-substring-search/zsh-history-substring-search.zsh
+# bind UP and DOWN arrow keys
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+
+# bind UP and DOWN arrow keys (compatibility fallback
+# for Ubuntu 12.04, Fedora 21, and MacOSX 10.9 users)
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+alias b=bundle
+alias be='bundle exec'
+alias emacs="emacs -nw"
+export CLICOLOR=1
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+eval "$(rbenv init -)"
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+autoload -U promptinit; promptinit
+prompt pure
+unsetopt nomatch
---
+++ zshrc
@@ -0,0 +1,4 @@
+export ZSH=$HOME/.oh-my-zsh
+plugins=(git zsh-completions)
+source $ZSH/oh-my-zsh.sh
+source ~/dotfiles/my-zsh.zsh
