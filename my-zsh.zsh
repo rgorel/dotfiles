9123b206b2e91c682c4ea5f980c35103906ca206
Roman Gorel <r.gorel@drooms.com>
2018-11-05 16:19:49 +0100

    some small amendments

--- my-zsh.zsh
+++ my-zsh.zsh
@@ -1,11 +1,11 @@
 export EDITOR=vim
 
-source ~/.oh-my-zsh/custom/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 
-source ~/.oh-my-zsh/custom/zsh-history-substring-search/zsh-history-substring-search.zsh
+#source ~/.oh-my-zsh/custom/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+#source ~/.oh-my-zsh/custom/zsh-history-substring-search/zsh-history-substring-search.zsh
 # bind UP and DOWN arrow keys
 zmodload zsh/terminfo
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
+#bindkey "$terminfo[kcuu1]" history-substring-search-up
+#bindkey "$terminfo[kcud1]" history-substring-search-down
 
 # bind UP and DOWN arrow keys (compatibility fallback
 # for Ubuntu 12.04, Fedora 21, and MacOSX 10.9 users)
@@ -33,6 +33,9 @@ alias dc="docker-compose"
 export CLICOLOR=1
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
+
+export FZF_DEFAULT_COMMAND='ag -l'
+alias fzfa="ag -lU|fzf"
 
 [[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
 autoload -U promptinit; promptinit
--- zshrc
+++ zshrc
@@ -1,6 +1,12 @@
+#START=$(gdate +%s.%N)
 export TERM='screen-256color'
 export ZSH=$HOME/.oh-my-zsh
 export ZSH_THEME=roman
 plugins=(git zsh-completions)
 source $ZSH/oh-my-zsh.sh
 source ~/dotfiles/my-zsh.zsh
+export HISTSIZE=1000
+export SAVEHIST=1000
+#END=$(gdate +%s.%N)
+#DIFF=$(echo "$END - $START" | bc)
+#echo $DIFF
