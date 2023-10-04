5e4f8ec4f31101d6f4882c72cc048416d41d8823
Roman Gorel <roman.gorel@flixbus.com>
2023-10-04 10:54:16 +0200

    nvim: add terraform syntax

--- nvim/lua/config/keys.lua
+++ nvim/lua/config/keys.lua
@@ -74,7 +74,7 @@ vim.keymap.set('n', '<leader>\\', require('telescope.builtin').commands, { desc 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'terraform' },
   
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
