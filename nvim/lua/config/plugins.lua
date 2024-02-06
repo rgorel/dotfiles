264f03cc81547f2ee9cea4f0cefe90ebe7bd2bbd
Roman Gorel <roman.gorel@flixbus.com>
2024-02-06 12:58:58 +0100

    nvim: relative path in inactive window

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -108,6 +108,15 @@ require('lazy').setup({
         lualine_y = {'progress'},
         lualine_z = {'location'}
       },
+
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {{'filename', path = 1}},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+      },
     },
   },
 
