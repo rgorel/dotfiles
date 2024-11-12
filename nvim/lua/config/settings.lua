0942642d5def47d2ee6d620a191e5a07a86af15e
Roman Gorel <roman.gorel@flix.com>
2024-11-12 11:58:47 +0100

    nvim indentation settings

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -49,5 +49,10 @@ vim.o.relativenumber = true
 vim.o.list = true
 vim.o.listchars = 'tab:» ,eol:¬,trail:⋅,extends:❯,precedes:❮'
 
+-- Indentation settings
 vim.o.smartindent = true
 vim.o.autoindent = true
+
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
